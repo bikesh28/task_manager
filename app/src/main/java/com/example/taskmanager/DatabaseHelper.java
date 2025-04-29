d5a84df63c028c3c718ee1178fb8e7e26cedb28f
Bikesh <bikeshsapkota12@gmail.com>
2025-04-29 19:18:38 +0545

    datbase helper

---
+++ app/src/main/java/com/example/taskmanager/DatabaseHelper.java
@@ -0,0 +1,25 @@
+package com.example.taskmanager;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "taskcollection.db";
+    public static final int DATABASE_VERSION = 1;
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
